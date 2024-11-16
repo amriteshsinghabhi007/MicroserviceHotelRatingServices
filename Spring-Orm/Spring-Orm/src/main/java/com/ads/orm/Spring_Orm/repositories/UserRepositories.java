d2e40fbf57cf148dc99dffec43388015df9678a0
Amritesh Singh <88612820+amriteshsinghabhi007@users.noreply.github.com>
2024-11-16 20:04:17 +0530

    Delete Spring-Orm/Spring-Orm directory

--- Spring-Orm/Spring-Orm/src/main/java/com/ads/orm/Spring_Orm/repositories/UserRepositories.java
+++
@@ -1,7 +0,0 @@
-package com.ads.orm.Spring_Orm.repositories;
-
-import com.ads.orm.Spring_Orm.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositories extends JpaRepository<User,Integer> {
-}
