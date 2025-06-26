838000968d572596d62ce146d1e6ccc3c4b842f5
Amritesh Singh <88612820+amriteshsinghabhi007@users.noreply.github.com>
2025-06-26 12:35:20 +0530

    Delete ElectronicStore/ElectronicStore directory

--- ElectronicStore/ElectronicStore/src/main/java/com/ads/electronic/store/ElectronicStore/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ads.electronic.store.ElectronicStore.repositories;
-
-import com.ads.electronic.store.ElectronicStore.dtos.CategoryDto;
-import com.ads.electronic.store.ElectronicStore.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,String> {
-}
